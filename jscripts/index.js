85bb78d5c6805de8a8c8a2b8624112a3555d18fc
ProjectO12 <projecttt012@gmail.com>
2024-10-10 10:20:52 +0530

    Update index.js

--- jscripts/index.js
+++ jscripts/index.js
@@ -10,7 +10,7 @@ function randomInRange(min, max) {
 
 	document.getElementById("container").style.width = "100vh";
 	
-	let bday = "October 10,2024 10:15:00",
+	let bday = "October 10,2024 10:25:00",
 	countdown = new Date(bday).getTime(),
 
 
